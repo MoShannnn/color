ad99e8bca15b8c8549a993c227e3229750a4d5eb
MoShannnn <moshan2500@gmail.com>
2024-07-25 20:55:09 +0630

    Added Login and Register Page

--- mycolor/views.py
+++ mycolor/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+def login(request):
+    return render(request, 'auth/login.html')
