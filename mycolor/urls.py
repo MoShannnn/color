95e5a7851cd946693dc96223b98fad0d26d9efec
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-07-28 00:32:16 +0630

    verify email ui

--- mycolor/urls.py
+++ mycolor/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.home, name="home"),
     path("register", views.register, name="register"),
+    path("verify-email/<slug:username>", views.verify_email, name="verify-email"),
     path("login", views.login, name="login"),
     path("searchEmail", views.searchEmail, name="searchEmail"),
     path("forgotPassword", views.forgotPassword, name="forgotPassword"),
--- mycolor/views.py
+++ mycolor/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-
+from django.utils import timezone
+from django.contrib import messages
 from .models import CustomUser
 
 # Create your views here.
@@ -30,17 +31,42 @@ def register(request):
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email, password)
+            user = CustomUser.objects.create_user(username, email, password)
             user.save()
+            messages.success(request, "Account created successfully! An OTP was sent to your Email")
         except IntegrityError:
             return render(request, "auth/register.html", {
                 "message": "Username already taken."
             })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+
+        return redirect("verify-email", username=request.POST['username'])
     else:
          return render(request, 'auth/register.html')
 
+def verify_email(request, username):
+    user = CustomUser.objects.get(username=username)
+    if request.method == "POST":
+        otp = request.POST['otp']
+        user_otp = user.otps.last()
+        if otp == user_otp.otp_code:
+
+            if user_otp.otp_expires_at < timezone.now():
+                return render(request, 'auth/verify_email.html', {
+                    'message': 'OTP has expired'
+                })
+            else:
+                user.is_active = True
+                user.save()
+                return redirect('login')
+        else:
+            messages.warning(request, "Invalid OTP entered, enter a valid OTP!")
+            return render(request, 'auth/verify_email.html', {
+
+            })
+    else:
+        return render(request, 'auth/verify_email.html', {
+            'username': username
+        })
 
 def login(request):
     return render(request, 'auth/login.html')
