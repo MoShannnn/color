cae1ce76307d08e08faf57f39ffd5a0096bd438f
KaungMinSett <KaungMinSat@my.uopeople.edu>
2024-08-26 22:26:02 +0630

    tables for palette created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Season(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=255)
+    hex = models.CharField(max_length=7)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="colors")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="colors")
+
+    def __str__(self):
+        return self.name
+
