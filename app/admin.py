2329d5647a21190d50e1355a025d8aea5f950bc1
KaungMinSett <KaungMinSat@my.uopeople.edu>
2024-08-26 22:26:26 +0630

    django admin registered

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from app.models import Color, Category, Season
 # Register your models here.
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hex', 'category', 'season')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Season, SeasonAdmin)
